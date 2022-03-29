94d15db169829cb0a765fa913ca4f17cf495282d
Philipp Holz <philippwood@gmx.net>
2022-03-29 15:10:13 +0200

    added route, routetable, igw

--- main.tf
+++ main.tf
@@ -4,11 +4,28 @@ resource "aws_vpc" "customer_vpc" {
     Name = "customer-vpc"
   }
 }
+resource "aws_internet_gateway" "gw" {
+    vpc_id = aws_vpc.customer_vpc.id
+}
 
 resource "aws_subnet" "customer_subnet" {
-  vpc_id = aws_vpc.customer_vpc.id
-  cidr_block = "110.0.0.0/24"
-  tags = {
+    vpc_id = aws_vpc.customer_vpc.id
+    cidr_block = "110.0.0.0/24"
+    tags = {
     Name = "customer-subnet"
   }
+}
+resource "aws_route_table" "customer_route_table" {
+  vpc_id = aws_vpc.customer_vpc.id
+
+  tags = {
+    Name = "public_route"
+  }
+}
+
+resource "aws_route" "prod-route-igw" {
+  route_table_id            = aws_route_table.customer_route_table.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id                = aws_internet_gateway.gw.id
+  depends_on                = [aws_route_table.customer_route_table]
 }
