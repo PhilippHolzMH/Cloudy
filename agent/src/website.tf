e9cbcede58abc0a7b3516c79f52306a833c4bd4c
Philipp Holz <philippwood@gmx.net>
2022-03-30 21:49:51 +0200

    added key-module, updated gitignore

--- agent/src/website.tf
+++ agent/src/website.tf
@@ -9,8 +9,12 @@ terraform {
 module "public-vpc" {
   source = "./module/public-vpc/"
 }
+
 module "public-ec2" {
   source        = "./module/public-ec2/"
   public_subnet = module.public-vpc.public_subnet
   public_sg     = module.public-vpc.public_sg
+}
+module "key" {
+  source = "./module/key/"  
 }
